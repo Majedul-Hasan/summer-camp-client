1d34d36b99a8fa969edf977ed4ad8d1bd7603f87
Majedul Hasan <hasanmajedul@gmail.com>
2023-06-08 05:19:47 +0600

    login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,7 +1,49 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import reading from '../../assets/Reading a book_Monochromatic.svg';
+import { useForm } from 'react-hook-form';
+import useAuth from '../../hooks/useAuth';
+import Swal from 'sweetalert2';
+import { useState } from 'react';
 
 const Login = () => {
+  const [firebaseError, setFirebaseError] = useState(null);
+  const { signIn } = useAuth();
+  const navigate = useNavigate();
+  const location = useLocation();
+  const from = location.state?.from?.pathname || '/';
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const swalWithBootstrapButtons = Swal.mixin({
+    customClass: {
+      confirmButton: 'btn btn-success',
+    },
+    buttonsStyling: false,
+  });
+
+  const onSubmit = (data) => {
+    console.log(data);
+    signIn(data.email, data.password)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+        swalWithBootstrapButtons.fire({
+          title: 'User Login Successful.',
+        });
+        navigate(from, { replace: true });
+      })
+      .catch((error) => {
+        const errorMessage = error.message;
+        setFirebaseError(errorMessage);
+
+        // ..
+      });
+  };
+
   return (
     <section className='h-screen flex flex-col md:flex-row justify-center space-y-10 md:space-y-0 md:space-x-16 items-center my-2 mx-5 md:mx-0 md:my-0'>
       <div className='md:w-1/3 max-w-sm'>
@@ -92,45 +134,73 @@ const Login = () => {
             Or
           </p>
         </div>
-        <input
-          className='text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded'
-          type='text'
-          placeholder='Email Address'
-        />
-        <input
-          className='text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded mt-4'
-          type='password'
-          placeholder='Password'
-        />
-        <div className='mt-4 flex justify-between font-semibold text-sm'>
-          <label className='flex text-slate-500 hover:text-slate-600 cursor-pointer'>
-            <input
-              className='mr-1'
-              type='checkbox'
-            />
-            <span>Remember Me</span>
-          </label>
-          <a
-            className='text-blue-600 hover:text-blue-700 hover:underline hover:underline-offset-4'
-            href='#'>
-            Forgot Password?
-          </a>
-        </div>
-        <div className='text-center md:text-left'>
-          <button
-            className='mt-4 bg-blue-600 hover:bg-blue-700 px-4 py-2 text-white uppercase rounded text-xs tracking-wider'
-            type='submit'>
-            Login
-          </button>
-        </div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <input
+            className='text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded'
+            type='email'
+            {...register('email', { required: true })}
+            placeholder='Email Address'
+          />
+          <input
+            className='text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded mt-4'
+            type='password'
+            {...register('password', {
+              required: true,
+              minLength: 6,
+              maxLength: 20,
+              pattern: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z])/,
+            })}
+            placeholder='Password'
+          />
+
+          <div className='mt-4 flex justify-between font-semibold text-sm'>
+            <label className='flex text-slate-500 hover:text-slate-600 cursor-pointer'>
+              <input
+                className='mr-1'
+                type='checkbox'
+              />
+              <span>Remember Me</span>
+            </label>
+            <a
+              className='text-blue-600 hover:text-blue-700 hover:underline hover:underline-offset-4'
+              href='#'>
+              Forgot Password?
+            </a>
+          </div>
+          <div className='text-center md:text-left'>
+            <button
+              className='mt-4 bg-blue-600 hover:bg-blue-700 px-4 py-2 text-white uppercase rounded text-xs tracking-wider'
+              type='submit'>
+              Login
+            </button>
+          </div>
+        </form>
         <div className='mt-4 font-semibold text-sm text-slate-500 text-center md:text-left'>
-          Don't have an account?{' '}
+          Do not have an account?{' '}
           <Link
             className='text-red-600 hover:underline hover:underline-offset-4'
             to='/signup'>
             Register
           </Link>
         </div>
+        {errors.password?.type === 'required' && (
+          <p className='text-red-600'>Password is required</p>
+        )}
+        {errors.password?.type === 'minLength' && (
+          <p className='text-red-600'>Password must be 6 characters</p>
+        )}
+        {errors.password?.type === 'maxLength' && (
+          <p className='text-red-600'>
+            Password must be less than 20 characters
+          </p>
+        )}
+        {errors.password?.type === 'pattern' && (
+          <p className='text-red-600'>
+            Password must have one Uppercase one lower case, one number and one
+            special character.
+          </p>
+        )}
+        {firebaseError && <p className='text-red-600'>{firebaseError}</p>}
       </div>
     </section>
   );
