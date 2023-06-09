ac6f7289eae6a93cbadf3ff9fcf09830e80567a8
Majedul Hasan <hasanmajedul@gmail.com>
2023-06-09 11:31:02 +0600

    register page

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -1,42 +1,76 @@
-import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import summer from '../../assets/Summer landscape_Monochromatic.svg';
 import { useForm } from 'react-hook-form';
 import useAuth from '../../hooks/useAuth';
 import { useState } from 'react';
 import SocialLogin from '../Shared/SocialLogin/SocialLogin';
 import { swalWithBootstrapButtons } from '../../util/swalWithBootstrapButtons';
+// import axiosInstance from '../../util/axiosInstance';
+import { imgHostingUrl } from '../../util/imgHostingUrl';
+import axiosInstance from '../../util/axiosInstance';
 
 const SignUp = () => {
   const [firebaseError, setFirebaseError] = useState(null);
-  const { createUser } = useAuth();
+  const { createUser, updateUserProfile } = useAuth();
   const navigate = useNavigate();
-  const location = useLocation();
-  const from = location.state?.from?.pathname || '/';
 
   const {
     register,
     handleSubmit,
     formState: { errors },
+    reset,
+    watch,
   } = useForm();
 
-  const onSubmit = (data) => {
-    console.log(data);
-    createUser(data.email, data.password)
-      .then((result) => {
-        const user = result.user;
-        console.log(user);
-        swalWithBootstrapButtons.fire({
-          title: 'User Login Successful.',
-        });
-        navigate(from, { replace: true });
-      })
-      .catch((error) => {
-        const errorMessage = error.message;
-        setFirebaseError(errorMessage);
+  console.log(errors);
 
-        // ..
+  const onSubmit = (data) => {
+    const formData = new FormData();
+    formData.append('image', data.image[0]);
+    console.log(data);
+
+    createUser(data.email, data.password).then((result) => {
+      const loggedUser = result.user;
+      console.log(loggedUser);
+      axiosInstance.post(imgHostingUrl, formData).then((imgResponse) => {
+        console.log(imgResponse);
+        if (imgResponse.data.success) {
+          const imgURL = imgResponse.data.display_url;
+          const { name, email, gender, address, phone } = data;
+          updateUserProfile(data.name, imgURL)
+            .then(() => {
+              const saveUser = {
+                name,
+                email,
+                gender,
+                address,
+                phone,
+              };
+              console.log('user profile info updated');
+              axiosInstance.post(`${import.meta.env.VITE_API}/users`, saveUser);
+              reset();
+              swalWithBootstrapButtons.fire({
+                position: 'center',
+                icon: 'success',
+                title: 'User created successfully.',
+                showConfirmButton: false,
+                timer: 1500,
+              });
+              navigate('/');
+            })
+            .catch((error) => {
+              const errorMessage = error.message;
+              setFirebaseError(errorMessage);
+
+              // ..
+            });
+        }
       });
+    });
   };
+  /*** */
+
+  ////////////////////////////////
 
   return (
     <section className='h-screen flex flex-col md:flex-row dark:text-blue-200 justify-center space-y-10 md:space-y-0 md:space-x-16 items-center my-2 mx-5 md:mx-0 md:my-0'>
@@ -84,6 +118,59 @@ const SignUp = () => {
             })}
             placeholder='Password'
           />
+          <input
+            className='text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded mt-4'
+            type='password'
+            placeholder='confirm password'
+            {...register('confirm_password', {
+              required: true,
+              validate: (val) => {
+                if (watch('password') != val) {
+                  return 'Your passwords do no match';
+                }
+              },
+            })}
+          />
+          <input
+            className='text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded mt-4'
+            type='text'
+            {...register('address', { required: true })}
+            placeholder='Your district, state'
+          />
+          <input
+            className='text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded mt-4'
+            type='text'
+            {...register('phone', { required: true })}
+            placeholder='Your phone number'
+          />
+          <label htmlFor='gender'> Gender</label>
+          <label htmlFor='gender'>
+            <input
+              {...register('gender', { required: true })}
+              type='radio'
+              name='gender'
+              defaultValue='male'
+            />{' '}
+            male{' '}
+          </label>
+          <label htmlFor='gender'>
+            <input
+              {...register('gender', { required: true })}
+              type='radio'
+              name='gender'
+              defaultValue='female'
+            />{' '}
+            female{' '}
+          </label>
+          <label htmlFor='gender'>
+            <input
+              {...register('gender', { required: true })}
+              type='radio'
+              name='gender'
+              defaultValue='other'
+            />{' '}
+            Other{' '}
+          </label>
 
           <div className='mt-4 flex justify-between font-semibold text-sm'></div>
           <div className='text-center md:text-left'>
@@ -105,8 +192,14 @@ const SignUp = () => {
         {errors.password?.type === 'required' && (
           <p className='text-red-600'>Password is required</p>
         )}
+        {errors.image?.type === 'required' && (
+          <p className='text-red-600'>image upload is required</p>
+        )}
         {errors.password?.type === 'minLength' && (
           <p className='text-red-600'>Password must be 6 characters</p>
+        )}
+        {errors.confirm_password?.type === 'validate' && (
+          <p className='text-red-600'>Your passwords do no match</p>
         )}
         {errors.password?.type === 'maxLength' && (
           <p className='text-red-600'>
--- src/util/axiosInstance.js
+++ src/util/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   //baseURL: 'http://localhost:9000',
-  baseURL: 'https://lwsserver-majedul.herokuapp.com',
+  baseURL: import.meta.env.VITE_API,
 });
 
 export default axiosInstance;
