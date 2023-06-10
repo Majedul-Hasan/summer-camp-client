9df877ea784eb963fe4d93c5f21222442d41e184
Majedul Hasan <hasanmajedul@gmail.com>
2023-06-10 17:15:42 +0600

    popular instructor  card on home page

---
+++ src/pages/Home/PopularInstructors/PopularInstructors.jsx
@@ -0,0 +1,21 @@
+import InstructorsCard from '../../Shared/Cards/InstructorsCard';
+
+const PopularInstructors = () => {
+  return (
+    <div className='py-10 border-t-2'>
+      <div className='text-center '>
+        <h2 className='text-2xl my-3 font-bold dark:text-orange-300 '>
+          Popular Courses
+        </h2>
+        <p className='dark:text-orange-100 '> How people join our courses</p>
+      </div>
+      <div className='grid grid-cols-1  gap-6 mx-2'>
+        <InstructorsCard />
+        <InstructorsCard />
+        <InstructorsCard />
+      </div>
+    </div>
+  );
+};
+
+export default PopularInstructors;
