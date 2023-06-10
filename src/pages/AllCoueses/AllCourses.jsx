9a2275533fa5e7bf3c2e66ebd187e4622a09ca7c
Majedul Hasan <hasanmajedul@gmail.com>
2023-06-10 14:10:09 +0600

    all course from db

--- src/pages/AllCoueses/AllCourses.jsx
+++ src/pages/AllCoueses/AllCourses.jsx
@@ -3,19 +3,20 @@ import { useLoaderData } from 'react-router-dom';
 import CourseCard from '../Shared/Cards/CourseCard';
 
 const AllCourses = () => {
-  const data = useLoaderData();
-  console.log(data);
+  const courses = useLoaderData();
+  console.log(courses);
   return (
     <div className='py-10'>
       <h2 className='font-bold  text-4xl text-center text-gray-600 dark:text-blue-200'>
         All Instructors
       </h2>
       <div className='grid grid-cols-1 lg:grid-cols-2 gap-6 mx-2'>
-        <CourseCard />
-        <CourseCard />
-        <CourseCard />
-        <CourseCard />
-        <CourseCard />
+        {courses?.map((course) => (
+          <CourseCard
+            key={course._id}
+            course={course}
+          />
+        ))}
       </div>
     </div>
   );
