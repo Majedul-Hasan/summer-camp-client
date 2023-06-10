7a558a614a8b58a5e117a839c839f4cd3c41e670
Majedul Hasan <hasanmajedul@gmail.com>
2023-06-11 00:03:37 +0600

    status update pending to active

---
+++ src/pages/Dashboard/AdminDashboard/ManageCourses/ManageCoursesActions.js
@@ -0,0 +1,29 @@
+import axiosInstance from '../../../../util/axiosInstance';
+import { swalWithBootstrapButtons } from '../../../../util/swalWithBootstrapButtons';
+const token = localStorage.getItem('school-access-token');
+
+const headers = {
+  Authorization: `Bearer ${token}`,
+};
+
+export const courseMakeActive = (course, refetch) => {
+  axiosInstance
+    .patch(
+      `${import.meta.env.VITE_API}/course/active/${course._id}`,
+      {},
+      { headers }
+    )
+    .then((data) => {
+      console.log(data);
+      if (data.data.modifiedCount) {
+        refetch();
+        swalWithBootstrapButtons.fire({
+          position: 'center',
+          icon: 'success',
+          title: `${course.name} is active Now!`,
+          showConfirmButton: false,
+          timer: 1500,
+        });
+      }
+    });
+};
