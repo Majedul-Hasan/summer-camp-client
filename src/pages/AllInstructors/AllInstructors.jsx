ad80d5ecdf0095de8e39b0cce27bf838899918fb
Majedul Hasan <hasanmajedul@gmail.com>
2023-06-10 19:49:41 +0600

    instructor page

--- src/pages/AllInstructors/AllInstructors.jsx
+++ src/pages/AllInstructors/AllInstructors.jsx
@@ -3,15 +3,20 @@ import { useLoaderData } from 'react-router-dom';
 import InstructorsCard from '../Shared/Cards/InstructorsCard';
 
 const AllInstructors = () => {
-  const data = useLoaderData();
-  console.log(data);
+  const instructors = useLoaderData();
+  console.log(instructors);
   return (
     <div className='py-10'>
-      <h2 className='font-bold  text-4xl text-center text-gray-600 dark:text-blue-200'>All Instructors</h2>
+      <h2 className='font-bold  text-4xl text-center text-gray-600 dark:text-blue-200'>
+        All Instructors
+      </h2>
       <div className='grid grid-cols-1  gap-6 mx-2'>
-        <InstructorsCard />
-        <InstructorsCard />
-        <InstructorsCard />
+        {instructors?.map((instructor) => (
+          <InstructorsCard
+            key={instructor._id}
+            instructor={instructor}
+          />
+        ))}
       </div>
     </div>
   );
