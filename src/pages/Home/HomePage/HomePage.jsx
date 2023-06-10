daa1de1744d4ab63c64b3c516a4197c8bd7c778d
Majedul Hasan <hasanmajedul@gmail.com>
2023-06-10 16:15:35 +0600

    popularCourses on home page

--- src/pages/Home/HomePage/HomePage.jsx
+++ src/pages/Home/HomePage/HomePage.jsx
@@ -3,14 +3,18 @@ import BannerSlider from '../BannerSlider/BannerSlider';
 import NewsLetter from '../NewsLetter/NewsLetter';
 import Testimonials from '../Testimonials/Testimonials';
 import Events from '../Events/Events';
+import PopularCourses from '../PopularCourses/PopularCourses';
+import { useLoaderData } from 'react-router-dom';
 
 const HomePage = () => {
+  const popularCourses = useLoaderData() || [];
   return (
     <>
       <Helmet>
         <title>summerCampLang | Home</title>
       </Helmet>
       <BannerSlider />
+      <PopularCourses popularCourses={popularCourses} />
       <Events />
       <Testimonials />
       <NewsLetter />
---
+++ src/pages/Home/PopularCourses/PopularCourses.jsx
@@ -0,0 +1,24 @@
+import CourseCard from '../../Shared/Cards/CourseCard';
+
+const PopularCourses = ({ popularCourses }) => {
+  return (
+    <div className='py-10 border-t-2'>
+      <div className='text-center '>
+        <h2 className='text-2xl my-3 font-bold dark:text-orange-300 '>
+          Popular Courses
+        </h2>
+        <p className='dark:text-orange-100 '> How people join our courses</p>
+      </div>
+      <div className='grid grid-cols-1 lg:grid-cols-2 gap-6 mx-2 mt-8'>
+        {popularCourses?.map((course) => (
+          <CourseCard
+            key={course._id}
+            course={course}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default PopularCourses;
