e39ce02321bbccec4d5503f56fef9da19de32a3d
Majedul Hasan <hasanmajedul@gmail.com>
2023-06-12 03:03:18 +0600

    define admin, instructor & student route

---
+++ src/Routes/StudentRoute..jsx
@@ -0,0 +1,25 @@
+import { Navigate, useLocation } from 'react-router';
+import useAuth from '../hooks/useAuth';
+import useAdmin from '../hooks/useAdmin';
+
+const InstructorRoute = ({ children }) => {
+  const { user, loading } = useAuth();
+  const [role, isAdminLoading] = useAdmin();
+  const location = useLocation();
+
+  if (loading || isAdminLoading) {
+    return <progress className='progress w-56'></progress>;
+  }
+
+  if (user && role?.role === 'student') {
+    return children;
+  }
+  return (
+    <Navigate
+      to='/'
+      state={{ from: location }}
+      replace></Navigate>
+  );
+};
+
+export default InstructorRoute;
