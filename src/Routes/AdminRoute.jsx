2b8e58ef348d1422745baf6cb94a716875e6292c
Majedul Hasan <hasanmajedul@gmail.com>
2023-06-12 02:59:44 +0600

    admin route modified

--- src/Routes/AdminRoute.jsx
+++ src/Routes/AdminRoute.jsx
@@ -4,14 +4,14 @@ import useAdmin from '../hooks/useAdmin';
 
 const AdminRoute = ({ children }) => {
   const { user, loading } = useAuth();
-  const [isAdmin, isAdminLoading] = useAdmin();
+  const [role, isAdminLoading] = useAdmin();
   const location = useLocation();
 
   if (loading || isAdminLoading) {
     return <progress className='progress w-56'></progress>;
   }
 
-  if (user && isAdmin) {
+  if (user && role?.role === 'admin') {
     return children;
   }
   return (
