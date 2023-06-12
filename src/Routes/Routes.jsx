0b8de00cc972188908b921e9ba85485cb69f854d
Majedul Hasan <hasanmajedul@gmail.com>
2023-06-12 13:40:38 +0600

    home page data leack from router

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -34,7 +34,6 @@ export const router = createBrowserRouter([
       {
         path: '/',
         element: <HomePage></HomePage>,
-        loader: () => fetch(`${import.meta.env.VITE_API}/courses?limit=6`),
       },
       {
         path: 'login',
