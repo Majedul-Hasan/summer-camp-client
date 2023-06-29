ad6d07646b45f07e2809a70e93a28badbb1e3f62
Majedul Hasan <hasanmajedul@gmail.com>
2023-06-30 00:14:13 +0600

    Provider in mainjsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,20 +7,24 @@ import './index.css';
 import { router } from './Routes/Routes.jsx';
 import AuthProvider from './providers/AuthProvider';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Provider } from 'react-redux';
+import { store } from './app/store';
 
 // Create a client
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AuthProvider>
-      <HelmetProvider>
-        <QueryClientProvider client={queryClient}>
-          <div className='max-w-screen-xl mx-auto '>
-            <RouterProvider router={router} />
-          </div>
-        </QueryClientProvider>
-      </HelmetProvider>
-    </AuthProvider>
+    <Provider store={store}>
+      <AuthProvider>
+        <HelmetProvider>
+          <QueryClientProvider client={queryClient}>
+            <div className='max-w-screen-xl mx-auto '>
+              <RouterProvider router={router} />
+            </div>
+          </QueryClientProvider>
+        </HelmetProvider>
+      </AuthProvider>
+    </Provider>
   </React.StrictMode>
 );
