a745156a336cb49d1b671f059648fd9138130962
Majedul Hasan <hasanmajedul@gmail.com>
2023-07-01 02:17:25 +0600

    instructors in ui

--- src/pages/AllInstructors/AllInstructors.jsx
+++ src/pages/AllInstructors/AllInstructors.jsx
@@ -1,40 +1,35 @@
-import { useLoaderData } from 'react-router-dom';
-
 import InstructorsCard from '../Shared/Cards/InstructorsCard';
 import { Helmet } from 'react-helmet-async';
+import { useGetAllInstructorsQuery } from '../../features/instructors/instructorsApi';
 
 const AllInstructors = () => {
-  const instructors = useLoaderData();
-  console.log(instructors);
+  const { isLoading, data: instructors } = useGetAllInstructorsQuery({});
+
   return (
     <>
-      {' '}
       <Helmet>
         <title>summer camp school | All Instructors</title>
       </Helmet>
-      <div className='py-10'>
-        <h2 className='font-bold  text-4xl text-center text-gray-600 dark:text-blue-200'>
-          All Instructors
-        </h2>
-        <div className='grid grid-cols-1  gap-6 mx-2'>
-          {instructors?.map((instructor) => (
-            <InstructorsCard
-              key={instructor._id}
-              instructor={instructor}
-            />
-          ))}
+
+      {isLoading ? (
+        <p>Loading ...</p>
+      ) : (
+        <div className='py-10'>
+          <h2 className='font-bold  text-4xl text-center text-gray-600 dark:text-blue-200'>
+            All Instructors
+          </h2>
+          <div className='grid grid-cols-1  gap-6 mx-2'>
+            {instructors?.map((instructor) => (
+              <InstructorsCard
+                key={instructor._id}
+                instructor={instructor}
+              />
+            ))}
+          </div>
         </div>
-      </div>
+      )}
     </>
   );
 };
 
 export default AllInstructors;
-/**+
- * 
- <div className='grid grid-cols-1 lg:grid-cols-2 gap-6 mx-2'>
-        <InstructorsCard />
-        <InstructorsCard />
-        <InstructorsCard />
-      </div> 
- */
--- src/pages/Home/PopularInstructors/PopularInstructors.jsx
+++ src/pages/Home/PopularInstructors/PopularInstructors.jsx
@@ -1,36 +1,34 @@
+import { useGetPopularInstructorsQuery } from '../../../features/instructors/instructorsApi';
 import InstructorsCard from '../../Shared/Cards/InstructorsCard';
-import { useQuery } from '@tanstack/react-query';
 
 const PopularInstructors = () => {
-  const { data: instructors = [] } = useQuery({
-    queryKey: ['instructor'],
-    queryFn: async () => {
-      const res = await fetch(
-        `${import.meta.env.VITE_API}/instructors?limit=6`
-      );
-      return res.json();
-    },
-  });
+  const { isLoading, data: instructors } = useGetPopularInstructorsQuery({});
 
   return (
-    <div className='py-10 border-t-2'>
-      <div className='text-center '>
-        <h2 className='text-2xl my-3 font-bold dark:text-orange-300 '>
-          Popular Instructors
-        </h2>
-        <p className='dark:text-orange-100 '>
-          How people loves their instructors
-        </p>
-      </div>
-      <div className='grid grid-cols-1   gap-6 mx-2'>
-        {instructors?.map((instructor) => (
-          <InstructorsCard
-            key={instructor._id}
-            instructor={instructor}
-          />
-        ))}
-      </div>
-    </div>
+    <>
+      {isLoading ? (
+        <p>loading .. ./. .. </p>
+      ) : (
+        <div className='py-10 border-t-2'>
+          <div className='text-center '>
+            <h2 className='text-2xl my-3 font-bold dark:text-orange-300 '>
+              Popular Instructors
+            </h2>
+            <p className='dark:text-orange-100 '>
+              How people loves their instructors
+            </p>
+          </div>
+          <div className='grid grid-cols-1   gap-6 mx-2'>
+            {instructors?.map((instructor) => (
+              <InstructorsCard
+                key={instructor._id}
+                instructor={instructor}
+              />
+            ))}
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
