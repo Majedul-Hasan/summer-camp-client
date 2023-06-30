3a8b879294d0b4e8dccf6047ef867df31ed1a11f
Majedul Hasan <hasanmajedul@gmail.com>
2023-06-30 17:35:34 +0600

    course api

---
+++ src/features/courses/coursesApi.js
@@ -0,0 +1,45 @@
+import { apiSlice } from '../api/apiSlice';
+import { allCourses, popularCourses } from './coursesSlice';
+
+export const courseApi = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    getAllCourses: builder.query({
+      query: ({ limit }) => {
+        const limitQ = limit ? `/courses?limit=${limit}` : '/courses';
+        return {
+          url: limitQ,
+          method: 'GET',
+        };
+      },
+      async onQueryStarted(arg, { queryFulfilled, dispatch }) {
+        try {
+          const result = await queryFulfilled;
+          console.log(result);
+          dispatch(allCourses(result.data));
+        } catch (error) {
+          console.log(error);
+        }
+      },
+    }),
+    getPopularCourses: builder.query({
+      query: ({ limit }) => {
+        const limitQ = limit ? `/courses?limit=${limit}` : '/courses';
+        return {
+          url: limitQ,
+          method: 'GET',
+        };
+      },
+      async onQueryStarted(arg, { queryFulfilled, dispatch }) {
+        try {
+          const result = await queryFulfilled;
+          console.log(result);
+          dispatch(popularCourses(result.data));
+        } catch (error) {
+          console.log(error);
+        }
+      },
+    }),
+  }),
+});
+
+export const { useGetAllCoursesQuery, useGetPopularCoursesQuery } = courseApi;
