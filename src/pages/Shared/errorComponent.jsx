dd4427e40d33b59274ed642ef097d47aa6f3a5a9
Majedul Hasan <hasanmajedul@gmail.com>
2023-06-30 11:17:27 +0600

    error component msg

---
+++ src/pages/Shared/errorComponent.jsx
@@ -0,0 +1,11 @@
+const errorComponent = ({ children }) => {
+  return (
+    <div className='flex items-center'>
+      <div className='relative bg-red-200 max-w-xl px-4 py-2 text-red-800 rounded shadow w-full'>
+        <span className='block text-sm'>{children}</span>
+      </div>
+    </div>
+  );
+};
+
+export default errorComponent;
